9aa897274a00a9ad120ccd610f551b5f4b8b1bed
Mahi107 <98629034+Mahi107@users.noreply.github.com>
2022-02-18 09:49:34 -0600

    Add files via upload

---
+++ assignment/a03/DOP853.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+from scipy.integrate import solve_ivp
+def dSdx(x, S):
+    x, v= S
+    return[v, -16*v -200(x)-10]
+X_0 = 0.4
+v_0 = 0
+S_0 = (x_0, v_0)
+t = np.linspace(0, 1, 100)
+sol= scipy.integrate.solve_ivp(dSdx, t_span=(0,max(t))), y0=[v0],method='DOP853', t_eval=t)
+x_sol = sol.T[0]
+v_sol = sol.T[1]
+plt.plot(t, x_sol)
+plt.plot(t, v_sol)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
